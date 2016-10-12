1bfe801d0db544d5cd7ff3edaa74540960fd5b35
Samuel Spencer <theodore.therone@gmail.com>
2016-10-12 02:39:15 +0000

    now with more dcat!

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires = [
-        'aristotle-metadata-registry>=1.3.0'
+        'aristotle-metadata-registry>=1.4.0'
     ]
 
 )
