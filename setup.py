f2663a5d44cf092cf0e0454cd482d6264f2f7d4e
Samuel Spencer <theodore.therone@gmail.com>
2016-12-01 03:50:11 +0000

    fix setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-aristotle-dataset-extensions',
     version='0.2',
-    packages=['aristotle_dse'],
+    packages=find_packages(),
     include_package_data=True,
     license='BSD Licence',
     description='',
