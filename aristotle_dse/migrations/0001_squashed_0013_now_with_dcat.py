1ab3b52fc9572697f6147b605d529aae3f1a1b11
Samuel Spencer <theodore.therone@gmail.com>
2017-03-15 10:27:43 +0000

    migrations :(

--- aristotle_dse/migrations/0001_squashed_0013_now_with_dcat.py
+++ aristotle_dse/migrations/0001_squashed_0013_now_with_dcat.py
@@ -2,14 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import ckeditor_uploader.fields
 
 
 class Migration(migrations.Migration):
 
+    replaces = [(b'aristotle_dse', '0001_initial'), (b'aristotle_dse', '0002_auto_20150510_0249'), (b'aristotle_dse', '0003_add_clusters_improve_names'), (b'aristotle_dse', '0004_auto_20160103_0200'), (b'aristotle_dse', '0005_improve_inculsion_help_text'), (b'aristotle_dse', '0006_auto_20160103_1855'), (b'aristotle_dse', '0007_add_ordering_to_inclusions'), (b'aristotle_dse', '0008_auto_20160318_2335'), (b'aristotle_dse', '0009_auto_20160725_1958'), (b'aristotle_dse', '0010_datasource_specification'), (b'aristotle_dse', '0011_auto_20160726_2015'), (b'aristotle_dse', '0012_fix_concept_fields'), (b'aristotle_dse', '0013_now_with_dcat')]
+
     dependencies = [
-        ('aristotle_mdr', '0017_add_organisations'),
         ('aristotle_mdr_identifiers', '0001_initial'),
-        ('aristotle_dse', '0012_fix_concept_fields'),
+        ('aristotle_mdr', '0013_concept_field_fixer_part1'),
+        ('aristotle_mdr', '0001_squashed_0012_better_workflows'),
+        ('aristotle_mdr', '0017_add_organisations'),
     ]
 
     operations = [
@@ -49,6 +53,20 @@ class Migration(migrations.Migration):
             bases=('aristotle_mdr._concept',),
         ),
         migrations.CreateModel(
+            name='DataSetSpecification',
+            fields=[
+                ('_concept_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='aristotle_mdr._concept')),
+                ('ordered', models.BooleanField(default=False, help_text='Indicates if the ordering for a dataset is must match exactly the order laid out in the specification.')),
+                ('collection_method', ckeditor_uploader.fields.RichTextUploadingField(blank=True)),
+                ('implementation_start_date', models.DateField(null=True, blank=True)),
+                ('implementation_end_date', models.DateField(null=True, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('aristotle_mdr._concept',),
+        ),
+        migrations.CreateModel(
             name='Distribution',
             fields=[
                 ('_concept_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='aristotle_mdr._concept')),
@@ -62,6 +80,7 @@ class Migration(migrations.Migration):
                 ('media_type', models.CharField(help_text='The media type of the distribution as defined by IANA.', max_length=512)),
                 ('format_type', models.CharField(help_text='The file format of the distribution.', max_length=512)),
                 ('publisher', models.ForeignKey(blank=True, to='aristotle_mdr.Organization', help_text='An entity responsible for making the dataset available.', null=True)),
+                ('specification', models.ForeignKey(blank=True, to='aristotle_dse.DataSetSpecification', help_text='The dataset specification to which this data source conforms', null=True)),
             ],
             options={
                 'abstract': False,
@@ -81,26 +100,56 @@ class Migration(migrations.Migration):
                 'ordering': ['order'],
             },
         ),
-        migrations.RemoveField(
-            model_name='datasource',
-            name='_concept_ptr',
+        migrations.CreateModel(
+            name='DSSClusterInclusion',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('maximum_occurances', models.PositiveIntegerField(default=1, help_text='The maximum number of times a item can be included in a dataset')),
+                ('cardinality', models.CharField(default='conditional', help_text='Specifies if a field is required, optional or conditional within a dataset based on this specification.', max_length=20, choices=[('optional', 'Optional'), ('conditional', 'Conditional'), ('mandatory', 'Mandatory')])),
+                ('specific_information', ckeditor_uploader.fields.RichTextUploadingField(help_text='Any additional information on the inclusion of a data element or cluster in a dataset.', blank=True)),
+                ('conditional_obligation', models.TextField(help_text='If an item is present conditionally, this field defines the conditions under which an item will appear.', blank=True)),
+                ('order', models.PositiveSmallIntegerField(help_text='If a dataset is ordered, this indicates which position this item is in a dataset.', null=True, verbose_name='Position', blank=True)),
+                ('child', models.ForeignKey(related_name='parent_dss', to='aristotle_dse.DataSetSpecification')),
+                ('dss', models.ForeignKey(to='aristotle_dse.DataSetSpecification')),
+            ],
+            options={
+                'ordering': ['order'],
+                'abstract': False,
+                'verbose_name': 'DSS Cluster Inclusion',
+            },
         ),
-        migrations.RemoveField(
-            model_name='datasource',
-            name='specification',
-        ),
-        migrations.AlterField(
-            model_name='datasetspecification',
-            name='ordered',
-            field=models.BooleanField(default=False, help_text='Indicates if the ordering for a dataset is must match exactly the order laid out in the specification.'),
-        ),
-        migrations.DeleteModel(
-            name='DataSource',
+        migrations.CreateModel(
+            name='DSSDEInclusion',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('maximum_occurances', models.PositiveIntegerField(default=1, help_text='The maximum number of times a item can be included in a dataset')),
+                ('cardinality', models.CharField(default='conditional', help_text='Specifies if a field is required, optional or conditional within a dataset based on this specification.', max_length=20, choices=[('optional', 'Optional'), ('conditional', 'Conditional'), ('mandatory', 'Mandatory')])),
+                ('specific_information', ckeditor_uploader.fields.RichTextUploadingField(help_text='Any additional information on the inclusion of a data element or cluster in a dataset.', blank=True)),
+                ('conditional_obligation', models.TextField(help_text='If an item is present conditionally, this field defines the conditions under which an item will appear.', blank=True)),
+                ('order', models.PositiveSmallIntegerField(help_text='If a dataset is ordered, this indicates which position this item is in a dataset.', null=True, verbose_name='Position', blank=True)),
+                ('data_element', models.ForeignKey(related_name='dssInclusions', to='aristotle_mdr.DataElement')),
+                ('dss', models.ForeignKey(to='aristotle_dse.DataSetSpecification')),
+            ],
+            options={
+                'ordering': ['order'],
+                'abstract': False,
+                'verbose_name': 'DSS Data Element Inclusion',
+            },
         ),
         migrations.AddField(
-            model_name='distribution',
-            name='specification',
-            field=models.ForeignKey(blank=True, to='aristotle_dse.DataSetSpecification', help_text='The dataset specification to which this data source conforms', null=True),
+            model_name='datasetspecification',
+            name='clusters',
+            field=models.ManyToManyField(to='aristotle_dse.DataSetSpecification', null=True, through='aristotle_dse.DSSClusterInclusion', blank=True),
+        ),
+        migrations.AddField(
+            model_name='datasetspecification',
+            name='data_elements',
+            field=models.ManyToManyField(to='aristotle_mdr.DataElement', null=True, through='aristotle_dse.DSSDEInclusion', blank=True),
+        ),
+        migrations.AddField(
+            model_name='datasetspecification',
+            name='statistical_unit',
+            field=models.ForeignKey(related_name='statistical_unit_of', blank=True, to='aristotle_mdr._concept', help_text='Indiciates if the ordering for a dataset is must match exactly the order laid out in the specification.', null=True),
         ),
         migrations.AddField(
             model_name='distribution',
