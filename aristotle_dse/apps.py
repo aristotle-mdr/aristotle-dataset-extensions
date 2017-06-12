48d6b7b00ba48c9731707be934ca3af29c087aa8
Samuel Spencer <theodore.therone@gmail.com>
2017-06-12 00:38:42 +0000

    ups for 1.5.5

--- aristotle_dse/apps.py
+++ aristotle_dse/apps.py
@@ -4,4 +4,4 @@ from aristotle_mdr.apps import AristotleExtensionBaseConfig
 class AristotleDSEConfig(AristotleExtensionBaseConfig):
     name = 'aristotle_dse'
     verbose_name = "Aristotle Dataset Extensions"
-    description = "Aristotle Dataset Extensions adds two content types for tracking data set defintions and data sources."
+    description = "Aristotle Dataset Extensions adds content types for tracking data set defintions and data sources."
